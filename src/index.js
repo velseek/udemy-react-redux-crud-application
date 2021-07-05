9c97148cdc4daca60976918da6973fbcc28fc9d0
DESKTOP-UQS59R9\User <sikoku.ababa@gmail.com>
2021-07-05 10:02:05 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createState} from 'redux'
+import { Provider} from 'react-redux'
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducer from './reducers'
+import App from './components/App';
+
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
